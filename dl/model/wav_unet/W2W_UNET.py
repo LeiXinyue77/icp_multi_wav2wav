9ed3b03f2fec18c5189fc96077c2dd5f294e6a90
Lucky Lei <1263588941@qq.com>
2025-01-14 22:37:14 +0800

    feat:小论文unet tensorflow to pytorch version

--- dl/model/wav_unet/W2W_UNET.py
+++ dl/model/wav_unet/W2W_UNET.py
@@ -1,142 +1,86 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.init as init
 import torchsummary
 import onnx
 import netron
 
 
-class UNet1D(nn.Module):
-    def __init__(self):
-        super(UNet1D, self).__init__()
+class UNet(nn.Module):
+    def __init__(self, ngf=8):
+        super(UNet, self).__init__()
+        self.enc1 = self.conv_block(1, ngf, kernel_size=3)
+        self.enc2 = self.conv_block(ngf, ngf*2, kernel_size=3)
+        self.enc3 = self.conv_block(ngf*2, ngf*4, kernel_size=3)
+        self.enc4 = self.conv_block(ngf*4, ngf*8, kernel_size=3)
+        self.center = self.conv_block(ngf*8, ngf*16, kernel_size=3)
+        self.dec4 = self.conv_block(ngf*16, ngf*8, kernel_size=3)
+        self.dec3 = self.conv_block(ngf*8, ngf*4, kernel_size=3)
+        self.dec2 = self.conv_block(ngf*4, ngf*2, kernel_size=3)
+        self.dec1 = self.conv_block(ngf*2, ngf, kernel_size=3)
+        self.up_conv4 = nn.Conv1d(ngf*16, ngf*8, kernel_size=2, padding="same")
+        self.up_conv3 = nn.Conv1d(ngf*8, ngf*4, kernel_size=2, padding="same")
+        self.up_conv2 = nn.Conv1d(ngf*4, ngf*2, kernel_size=2, padding="same")
+        self.up_conv1 = nn.Conv1d(ngf*2, ngf, kernel_size=2, padding="same")
+        self.up_conv0 = nn.Conv1d(ngf, 2, kernel_size=3, padding="same")
+        self.final = nn.Conv1d(2, 1, kernel_size=3, padding="same")
 
-        # Contraction path
-        self.conv1 = nn.Sequential(
-            nn.Conv1d(1, 64, kernel_size=3, padding=1),
+        # 初始化所有卷积层的权重为 He_Normal
+        self.apply(self.init_weights)
+
+    def conv_block(self, in_channels, out_channels, kernel_size):
+        # padding = (kernel_size - 1) // 2  # 计算 padding 确保 'same' 填充
+        conv = nn.Sequential(
+            nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding="same"),
             nn.ReLU(inplace=True),
-            nn.Conv1d(64, 64, kernel_size=3, padding=1),
+            nn.Conv1d(out_channels, out_channels, kernel_size=kernel_size, padding="same"),
             nn.ReLU(inplace=True)
         )
-        self.pool1 = nn.MaxPool1d(kernel_size=2)
+        return conv
 
-        self.conv2 = nn.Sequential(
-            nn.Conv1d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
-        self.pool2 = nn.MaxPool1d(kernel_size=2)
-
-        self.conv3 = nn.Sequential(
-            nn.Conv1d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
-        self.pool3 = nn.MaxPool1d(kernel_size=2)
-
-        self.conv4 = nn.Sequential(
-            nn.Conv1d(256, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
-        self.drop4 = nn.Dropout(0.5)
-        self.pool4 = nn.MaxPool1d(kernel_size=2)
-
-        self.conv5 = nn.Sequential(
-            nn.Conv1d(512, 1024, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(1024, 1024, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
-        self.drop5 = nn.Dropout(0.5)
-
-        # Expansive path (反卷积替代上采样)
-        self.up6 = nn.ConvTranspose1d(1024, 512, kernel_size=2, stride=2, padding=0)
-        self.conv6 = nn.Sequential(
-            nn.Conv1d(1024, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
-
-        self.up7 = nn.ConvTranspose1d(512, 256, kernel_size=2, stride=2, padding=0)
-        self.conv7 = nn.Sequential(
-            nn.Conv1d(512, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
-
-        self.up8 = nn.ConvTranspose1d(256, 128, kernel_size=2, stride=2, padding=0)
-        self.conv8 = nn.Sequential(
-            nn.Conv1d(256, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
-
-        self.up9 = nn.ConvTranspose1d(128, 64, kernel_size=2, stride=2, padding=0)
-        self.conv9 = nn.Sequential(
-            nn.Conv1d(128, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
-        self.conv10 = nn.Conv1d(64, 2, kernel_size=3, padding=1)
-        self.final_conv = nn.Conv1d(2, 1, kernel_size=3, padding=1)
-        self.sigmoid = nn.Sigmoid()
+    def init_weights(self, m):
+        """ 对卷积层进行 He Normal 初始化 """
+        if isinstance(m, nn.Conv1d):
+            init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)  # 将偏置初始化为 0
 
     def forward(self, x):
-        x = x.double()
-        # Contraction path
-        conv1 = self.conv1(x)
-        pool1 = self.pool1(conv1)
+        _input = x.double()
+        enc1 = self.enc1(_input)
+        enc2 = self.enc2(nn.MaxPool1d(2)(enc1))
+        enc3 = self.enc3(nn.MaxPool1d(2)(enc2))
+        enc4 = self.enc4(nn.MaxPool1d(2)(enc3))
 
-        conv2 = self.conv2(pool1)
-        pool2 = self.pool2(conv2)
+        drop4 = nn.Dropout(0.5)(enc4)
+        center = self.center(nn.MaxPool1d(2)(drop4))
+        drop_center = nn.Dropout(0.5)(center)
 
-        conv3 = self.conv3(pool2)
-        pool3 = self.pool3(conv3)
+        # Decode with strict size matching
+        up4 = self.up_conv4(nn.Upsample(scale_factor=2, mode='nearest')(drop_center))
+        dec4 = self.dec4(torch.cat((up4, enc4), dim=1))
 
-        conv4 = self.conv4(pool3)
-        drop4 = self.drop4(conv4)
-        pool4 = self.pool4(drop4)
+        up3 = self.up_conv3(nn.Upsample(scale_factor=2, mode='nearest')(dec4))
+        dec3 = self.dec3(torch.cat((up3, enc3), dim=1))
 
-        conv5 = self.conv5(pool4)
-        drop5 = self.drop5(conv5)
+        up2 = self.up_conv2(nn.Upsample(scale_factor=2, mode='nearest')(dec3))
+        dec2 = self.dec2(torch.cat((up2, enc2), dim=1))
 
-        # Expansive path
-        up6 = self.up6(drop5)
-        merge6 = torch.cat((drop4, up6), dim=1)
-        conv6 = self.conv6(merge6)
+        up1 = self.up_conv1(nn.Upsample(scale_factor=2, mode='nearest')(dec2))
+        dec1 = self.dec1(torch.cat((up1, enc1), dim=1))
 
-        up7 = self.up7(conv6)
-        merge7 = torch.cat((conv3, up7), dim=1)
-        conv7 = self.conv7(merge7)
-
-        up8 = self.up8(conv7)
-        merge8 = torch.cat((conv2, up8), dim=1)
-        conv8 = self.conv8(merge8)
-
-        up9 = self.up9(conv8)
-        merge9 = torch.cat((conv1, up9), dim=1)
-        conv9 = self.conv9(merge9)
-
-        conv10 = self.conv10(conv9)
-        output = self.final_conv(conv10)
-        return self.sigmoid(output)
+        out = self.up_conv0(dec1)
+        return torch.sigmoid(self.final(out))
 
 
 # cpu版本测试
 if __name__ == "__main__":
-    model = UNet1D().double().to('cpu')
-    MRI = torch.randn(1, 1, 512).double().to('cpu')
+    model = UNet(ngf=8).double().to('cpu')
+    MRI = torch.randn(32, 1, 1024).double().to('cpu')
     print(MRI.dtype)
     predict = model(MRI)
     print(predict.shape)  # (bz, 1, 512)
-    torchsummary.summary(model, input_size=(1, 512), batch_size=1, device='cpu')
+    torchsummary.summary(model, input_size=(1, 1024), batch_size=32, device='cpu')
 
     print("====================================== model summary finished !!!========================================")
 
